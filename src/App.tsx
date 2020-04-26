0402645c863a486fcf346b4f3136024a4973a8de
haisheng-lin <linhaicheng@xiao100.com>
2020-04-26 17:08:47 +0800

    refactor: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+
+import 'assets/styles/reset.scss';
+import 'assets/styles/base.scss';
+
+import Home from 'pages/Home';
 
 const App: React.FC = () => {
-  return <div>hello resume</div>;
+  return <Home />;
 };
 
 export default App;
