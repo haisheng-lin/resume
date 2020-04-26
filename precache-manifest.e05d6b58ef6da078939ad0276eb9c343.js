2c6ba07c6007167841f787ac945715f6cbf9bba1
haisheng-lin <linhaicheng@xiao100.com>
2020-04-26 17:27:12 +0800

    Updates

---
+++ precache-manifest.e05d6b58ef6da078939ad0276eb9c343.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e12704118b966da6e9b425e1cdabe6",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "2d48eafe5499bdabfd36",
+    "url": "/resume/static/css/2.e9f00c44.chunk.css"
+  },
+  {
+    "revision": "c7673e53549365681a15",
+    "url": "/resume/static/css/main.d95febe2.chunk.css"
+  },
+  {
+    "revision": "2d48eafe5499bdabfd36",
+    "url": "/resume/static/js/2.7eac9b12.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/resume/static/js/2.7eac9b12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7673e53549365681a15",
+    "url": "/resume/static/js/main.f5034ac2.chunk.js"
+  },
+  {
+    "revision": "6a13bab08944207abc31",
+    "url": "/resume/static/js/runtime-main.486ca00e.js"
+  },
+  {
+    "revision": "aa62ffb54ae4d36638ed35793486e331",
+    "url": "/resume/static/media/avatar.aa62ffb5.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/resume/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
