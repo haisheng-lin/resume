334bc967956e76d82e9143601576bbad8245c51c
haisheng-lin <hlin86@asu.edu>
2023-01-26 12:14:39 +0800

    refactor: 去除 SVGIcon prefix prop

--- src/components/svg-icon.vue
+++ src/components/svg-icon.vue
@@ -5,17 +5,15 @@ const props = withDefaults(
   defineProps<{
     name: string;
     size?: number;
-    prefix?: string;
     fill?: string;
   }>(),
   {
     size: 20,
-    prefix: 'icon',
     fill: '#333'
   }
 );
 
-const symbolId = computed(() => `#${props.prefix}-${props.name}`);
+const symbolId = computed(() => `#icon-${props.name}`);
 </script>
 
 <template>
