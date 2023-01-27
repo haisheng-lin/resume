20f3dd8e9bf9f21cc53de0fa5b61877aab51f0f4
Haisheng Lin <hlin86@asu.edu>
2023-01-27 20:08:22 +0800

    chore: 优化打包与 i18n (#30)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 import path from 'path';
 
 // https://vitejs.dev/config/
@@ -12,6 +13,10 @@ export default defineConfig({
       iconDirs: [path.resolve(__dirname, 'src/assets/images/icons')],
       // 指定 symbolId 格式
       symbolId: 'icon-[dir]-[name]'
+    }),
+    VueI18nPlugin({
+      // locale messages resource pre-compile option
+      include: path.resolve(__dirname, 'src/i18n/locales/**')
     })
   ],
   resolve: {
@@ -19,5 +24,17 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src')
     }
   },
-  base: '/resume'
+  base: '/resume',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // 将第三方包全部打包在一个 chunk 中，名称叫 vendor
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
