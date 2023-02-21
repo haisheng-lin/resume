387ede895c51ac095e5ffa8bb2d680c8313fe46a
Haisheng Lin <hlin86@asu.edu>
2023-02-21 10:50:40 +0800

    feat: 适配移动端 (#37)

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,11 @@
 <script setup lang="ts">
-import Resume from '@/components/resume/index.vue';
-import SVGIcon from '@/components/svg-icon.vue';
-import { resume } from '@/config';
-import useTheme from '@/hooks/useTheme';
+import PC from './pc/index.vue';
+import Mobile from './mobile/index.vue';
 
-const { isDarkTheme, switchTheme } = useTheme();
-
-function handlePrint() {
-  window.print();
-}
+const usePC = document.body.clientWidth >= 800;
 </script>
 
 <template>
-  <div class="app-container">
-    <header class="app-header">
-      <SVGIcon
-        class="app-ops--item"
-        fill="var(--text-color)"
-        :name="isDarkTheme ? 'sun' : 'moon'"
-        @click="switchTheme"
-      />
-      <SVGIcon
-        class="app-ops--item"
-        fill="var(--text-color)"
-        name="print"
-        @click="handlePrint"
-      />
-    </header>
-    <main class="app-main">
-      <Resume :resume="resume" />
-    </main>
-  </div>
+  <PC v-if="usePC" />
+  <Mobile v-else />
 </template>
-
-<style lang="less" scoped>
-.app-container {
-  padding: 0 24px;
-  background-color: var(--background-color);
-  color: var(--text-color);
-  transition: all 0.2s;
-}
-.app-header {
-  display: flex;
-  justify-content: flex-end;
-  padding: 8px 0;
-}
-.app-main {
-  padding: 24px 0;
-}
-.app-ops--item {
-  margin-left: 12px;
-  cursor: pointer;
-  &:first-child {
-    margin-left: 0;
-  }
-}
-
-/* 打印时样式 */
-@media print {
-  @page {
-    margin: 0;
-  }
-  .app-header {
-    display: none;
-  }
-  .app-container,
-  .app-main {
-    padding: 0;
-  }
-}
-</style>
---
+++ src/mobile/section.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import SVGIcon from '@/components/svg-icon.vue';
+
+defineProps<{
+  icon?: string;
+  title: string;
+}>();
+</script>
+
+<template>
+  <section class="resume-section">
+    <div class="section-header">
+      <div class="section-header--container">
+        <SVGIcon
+          v-if="icon"
+          :name="icon"
+          :size="16"
+          fill="var(--text-color-secondary)"
+        />
+        <h2 class="section-header--title">{{ title }}</h2>
+      </div>
+    </div>
+    <slot />
+  </section>
+</template>
+
+<style lang="less" scoped>
+@import '@/assets/styles/vars.less';
+.resume-section {
+  margin-top: 8px;
+  &:first-child {
+    margin-top: 0;
+  }
+}
+.section-header {
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+  padding-bottom: 8px;
+  border-bottom: 1px solid #e0e0e0;
+}
+.section-header--container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: @font-size-large;
+  background-color: var(--primary-color);
+  color: var(--text-color-secondary);
+  border-radius: 4px;
+}
+.section-header--title {
+  margin-left: 12px;
+  font-weight: bold;
+}
+</style>
