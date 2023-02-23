5754583b9b3d1690d00a39a754a7a635195a3072
Haisheng Lin <hlin86@asu.edu>
2023-02-23 23:52:01 +0800

    feat: update resume (#38)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,85 @@
 <script setup lang="ts">
-import PC from './pc/index.vue';
-import Mobile from './mobile/index.vue';
+import SVGIcon from '@/components/svg-icon.vue';
+import { resume } from '@/config';
+import useTheme from '@/hooks/useTheme';
+import WaterfallResume from '@/templates/waterfall/index.vue';
 
-const usePC = document.body.clientWidth >= 800;
+const { isDarkTheme, switchTheme } = useTheme();
+
+function handlePrint() {
+  window.print();
+}
 </script>
 
 <template>
-  <PC v-if="usePC" />
-  <Mobile v-else />
+  <div class="app-container">
+    <header class="app-header">
+      <SVGIcon
+        class="app-ops--item"
+        fill="var(--text-color)"
+        :name="isDarkTheme ? 'sun' : 'moon'"
+        @click="switchTheme"
+      />
+      <SVGIcon
+        class="app-ops--item"
+        fill="var(--text-color)"
+        name="print"
+        @click="handlePrint"
+      />
+    </header>
+    <main class="app-main">
+      <WaterfallResume :resume="resume" />
+    </main>
+  </div>
 </template>
+
+<style lang="less" scoped>
+@import '@/assets/styles/vars.less';
+.app-container {
+  padding: 0 24px;
+  background-color: var(--background-color);
+  color: var(--text-color);
+  transition: all 0.2s;
+}
+.app-header {
+  display: flex;
+  justify-content: flex-end;
+  padding: 8px 0;
+}
+.app-main {
+  padding: 24px 0;
+}
+.app-ops--item {
+  margin-left: 12px;
+  cursor: pointer;
+  &:first-child {
+    margin-left: 0;
+  }
+}
+
+@media screen and (max-width: @pc-min-width) {
+  .app-container {
+    padding: 0 12px;
+  }
+  .app-header {
+    display: none;
+  }
+  .app-main {
+    padding: 12px 0;
+  }
+}
+
+/* 打印时样式 */
+@media print {
+  @page {
+    margin: 0;
+  }
+  .app-header {
+    display: none;
+  }
+  .app-container,
+  .app-main {
+    padding: 0;
+  }
+}
+</style>
